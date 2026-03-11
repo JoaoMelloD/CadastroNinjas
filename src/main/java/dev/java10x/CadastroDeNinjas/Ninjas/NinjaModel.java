76b08729c062593fdbfda22a478f700e61c8afa0
JoaoMeloD <joao.delgado@edu.unipar.br>
2026-03-11 15:15:42 -0300

    Ninja Repository

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -21,6 +21,7 @@ public class NinjaModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
     private String name;
+    @Column(unique = true)
     private String email;
     private int idade;
 
